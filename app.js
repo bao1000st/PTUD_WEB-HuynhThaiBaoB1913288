03dea280a5e5c98343f35f96fe66f13d03dffb18
bao1000st <bao1000st@gmail.com>
2022-08-16 22:59:06 +0700

    dinh nghia cac route cho tai nguyen contact

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express=require("express");
 const cors=require("cors");
 
 const app = express();
+const contactRouter = require("./app/routes/contact.route");
+
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res)=> {
-    res.json({message: "welcome to contact book application."});
-});
+// app.get("/", (req, res)=> {
+//     res.json({message: "welcome to contact book application."});
+// });
+
+app.use("api/contacts",contactRouter);
 
 module.exports = app;
