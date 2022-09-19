73fea04cb744c682cfbc37fd3b1a41c2b52f6bfc
bao1000st <bao1000st@gmail.com>
2022-09-19 12:58:39 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -13,17 +13,17 @@ app.get("/", (req, res) => {
 
 app.use("/api/contacts", contactRouter);
 
-// // handle 404 response
-// app.use((req, res, next) => {
-//     return next(new ApiError(404, "Resource not found"));
-// });
+// handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
 
-// // define error-handling middlware last, after other app.use() and routes calls
-// app.use((err, req, res, next) => {
-//     return res.status(err.statusCode || 500).json({
-//         message: err.message || "Internal Server Error",
-//     });
-// });
+// define error-handling middlware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 
 
 
